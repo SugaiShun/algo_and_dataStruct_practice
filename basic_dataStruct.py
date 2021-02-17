09e2c3dde686d470b5a3403663ac4bfe3e4ec426
shun <shun9121@gmail.com>
2021-02-18 01:25:35 +0900

    queue code

--- basic_dataStruct.py
+++ basic_dataStruct.py
@@ -4,7 +4,7 @@
 
 class stack:
     """
-    python's stack class.
+    original stack class.
     """
     def __init__(self):
         self.stack = []
@@ -16,5 +16,55 @@ class stack:
         return self.stack==[]
     def size(self):
         return len(self.stack)
+
+class queue:
+    """
+    original queue class.
+    """
+    def __init__(self):
+        self.queue = []
+    def is_empty(self):
+        return self.queue==[]
+    def enqueue(self,item):
+        return self.queue.append(item)
+    def dequeue(self):
+        return self.queue.pop(-1)
+    def size(self):
+        return len(self.queue)
+
+class queue_likeC:
+    """
+    original queue class.
+    """
+    def __init__(self, size):
+        self.head = 0
+        self.tail = 0
+        self.size = size
+        self.data = [0]*self.size
+    def is_empty(self):
+        return self.head == self.tail
+    def is_full(self):
+        return self.head == (self.tail + 1) % self.size
+    def enqueue(self,item):
+        if self.is_full():
+            return print("overflow.queue.")
+        self.data[self.tail] = item
+        if self.tail + 1 == self.size:
+            self.tail = 0
+        else:
+            self.tail += 1
+    def dequeue(self):
+        if self.is_empty():
+            return print("underflow.queue.")
+
+        item = self.data[self.head]            
+        if self.head + 1 == self.size:
+            self.head = 0
+        else:
+            self.head += 1
+        return item
+        
+
+
 
     
